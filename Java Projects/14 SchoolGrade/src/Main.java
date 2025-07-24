0e7d7a0e78c34660c0f93452a9a00c8281413d29
Shahidul Alam <131958399+shahidulq@users.noreply.github.com>
2025-07-24 16:33:07 +0600

    SchoolGrade project added

---
+++ Java Projects/14 SchoolGrade/src/Main.java
@@ -0,0 +1,21 @@
+public class Main {
+    public static void main(String[] args) {
+
+        String dollyGrade = SchoolGrade.getGrade(75);
+        String mollyGrade = SchoolGrade.getGrade(65);
+        String pollyGrade = SchoolGrade.getGrade(85);
+        String mishaGrade = SchoolGrade.getGrade(55);
+        String bishopGrade = SchoolGrade.getGrade(88);
+        String nishiGrade = SchoolGrade.getGrade(82);
+
+        System.out.printf("""
+                        Dolly got: %s
+                        Molly got: %s
+                        Polly got: %s
+                        Misha got: %s
+                        Bishop got: %s
+                        Nishi got: %s
+                        """,
+                dollyGrade, mollyGrade, pollyGrade, mishaGrade, bishopGrade, nishiGrade);
+    }
+}
--- Java Projects/14 SchoolGrade/src/SchoolGrade.java
+++ Java Projects/14 SchoolGrade/src/SchoolGrade.java
@@ -1,4 +1,40 @@
-package PACKAGE_NAME;
-
 public enum SchoolGrade {
+    A(90, 100),
+    B(80, 90),
+    C(70, 80),
+    D(60, 70),
+    E(50, 60),
+    F(0,50);
+
+    private final int minRange;
+    private final int maxRange;
+
+    SchoolGrade(int minRange, int maxRange) {
+        this.minRange = minRange;
+        this.maxRange = maxRange;
+    }
+
+    public boolean isInRange(int score){
+        return score >= minRange && score <= maxRange;
+    }
+
+    public static String getGrade(int studentScore) {
+        String res;
+        if (SchoolGrade.A.isInRange(studentScore)){
+            res = SchoolGrade.A.name();
+        } else if (SchoolGrade.B.isInRange(studentScore)){
+            res = SchoolGrade.B.name();
+        } else if (SchoolGrade.C.isInRange(studentScore)){
+            res = SchoolGrade.C.name();
+        } else if (SchoolGrade.D.isInRange(studentScore)){
+            res = SchoolGrade.D.name();
+        } else if (SchoolGrade.E.isInRange(studentScore)){
+            res = SchoolGrade.E.name();
+        } else if (SchoolGrade.F.isInRange(studentScore)){
+            res = SchoolGrade.F.name();
+        } else {
+            res = "Number not between 0 and 100";
+        }
+        return res;
+    }
 }
